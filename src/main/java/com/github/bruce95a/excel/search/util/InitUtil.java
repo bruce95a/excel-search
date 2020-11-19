d5a13bccf92a29b66342b767e95afa4c1613f93a
bruce95a <bruce95a@hotmail.com>
2020-11-19 12:35:28 +0800

    init database

---
+++ src/main/java/com/github/bruce95a/excel/search/util/InitUtil.java
@@ -0,0 +1,32 @@
+package com.github.bruce95a.excel.search.util;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+
+@Component
+public class InitUtil {
+    private static final Logger logger = LoggerFactory.getLogger(InitUtil.class);
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @PostConstruct
+    public void init() {
+        logger.info("init database");
+        jdbcTemplate.execute("CREATE TABLE IF NOT EXISTS item (" +
+                "batch varchar(255)," +
+                "industry varchar(255)," +
+                "category varchar(255)," +
+                "health varchar(255)," +
+                "code varchar(255)," +
+                "name varchar(255)," +
+                "scope varchar(255)," +
+                "logout varchar(255)," +
+                "status varchar(255) );");
+    }
+}
