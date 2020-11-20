610f0a87c67bdbb7c4c24735197fc0c87a7b6331
bruce95a <bruce95a@hotmail.com>
2020-11-20 23:24:56 +0800

    remove FontAwesome

--- README.md
+++ README.md
@@ -9,6 +9,5 @@ Read excel file and save its data to a database. Search througn SQL and show the
 * H2 database
 * Apache POI
 * w3.css
-* FontAwesome
 * vue.js
 * axios.js
--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -56,13 +56,3 @@ var app = new Vue({
         }
     }
 });
-
-function w3_open() {
-    document.getElementById("mySidebar").style.display = "block";
-    document.getElementById("myOverlay").style.display = "block";
-}
-
-function w3_close() {
-    document.getElementById("mySidebar").style.display = "none";
-    document.getElementById("myOverlay").style.display = "none";
-}
