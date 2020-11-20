d642395f8575f07a51abacca98296836b39aa034
bruce95a <bruce95a@hotmail.com>
2020-11-21 00:34:41 +0800

    format

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -3,56 +3,57 @@ var app = new Vue({
   data: {
     keyword: '',
     info: {
-        items: [{
-            batch: '',
-            industry: '',
-            category: '',
-            health: '',
-            code: '',
-            name: '',
-            scope: '',
-            logout: '',
-            status: ''
-        }],
-        total: 1, // total page
-        page: 1, // page index
-        size: 1 // page size
+      items: [{
+        batch: '',
+        industry: '',
+        category: '',
+        health: '',
+        code: '',
+        name: '',
+        scope: '',
+        logout: '',
+        status: ''
+      }],
+      page: 1, // page index
+      size: 10, // page size
+      total: 1, // total page
+      totalNm: 0 // total count
     }
   },
   mounted() {
     axios.get('items').then(response => (this.info = response.data));
   },
-    methods:{
-        cancelFun: function(){
-            document.getElementById("frameId").style.display = "none";
-        },
-        sureFun: function(index){
-            axios.get('reload').then(response => (this.info = response.data));
-            document.getElementById("frameId").style.display = "none";
+  methods: {
+    cancelFun: function () {
+      document.getElementById("frameId").style.display = "none";
+    },
+    sureFun: function (index) {
+      axios.get('reload').then(response => (this.info = response.data));
+      document.getElementById("frameId").style.display = "none";
 
-        },
-        reLoadFun: function(index){
-            document.getElementById("frameId").style.display = "block";
-        },
-        find: function() {
-            axios.get('items', {
-                 params: {
-                   q: this.keyword,
-                   p: 1
-                 }
-             })
-             .then(response => (this.info = response.data));
-        },
-        page: function(arg) {
-             var page = app.info.page + arg;
-             page = page > 0 ? page : 1;
-             axios.get('items', {
-                  params: {
-                    q: this.keyword,
-                    p: page
-                  }
-              })
-              .then(response => (this.info = response.data));
+    },
+    reLoadFun: function (index) {
+      document.getElementById("frameId").style.display = "block";
+    },
+    find: function () {
+      axios.get('items', {
+        params: {
+          q: this.keyword,
+          p: 1
         }
+      })
+        .then(response => (this.info = response.data));
+    },
+    page: function (arg) {
+      var page = app.info.page + arg;
+      page = page > 0 ? page : 1;
+      axios.get('items', {
+        params: {
+          q: this.keyword,
+          p: page
+        }
+      })
+        .then(response => (this.info = response.data));
     }
+  }
 });
